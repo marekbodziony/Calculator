c50528d7a4c795cad9b4d4b960f7f4706156fd47
Marek Bodziony <marek_bodziony@o2.pl>
2016-11-18 17:11:44 +0100

    Adding two digits is working (a+b), as well as "clear"

--- Calculator/src/atj/Calc.java
+++ Calculator/src/atj/Calc.java
@@ -6,15 +6,14 @@ public class Calc {
 	private float b = 0;
 	private String result;
 	private String resultDetails;
-	private String action;
+	//private String action;
 	
-	// calculation
-	public String calculate (String action){
-		if (action.equals("+")){
-			result = "" + a + b;
-		}			
-		return result = action;
+	// adding
+	public String add(){
+		a = a + b;
+		return result = "" + a;
 	}
+	// 
 	
 	
 	// getter
@@ -22,12 +21,12 @@ public class Calc {
 	public float getB() { return b;}
 	public String getResult() {return result;}
 	public String getResultDetails() { return resultDetails;}
-	public String getAction() { return action;}
+	//public String getAction() { return action;}
 	
 	// setter
 	public void setA(float a) { this.a = a;}
 	public void setB(float b) { this.b = b;}
 	public void setResult(String result) { this.result = result;}
 	public void setResultDetails(String resultDetails) { this.resultDetails = resultDetails;}
-	public void setAction(String action) { this.action = action;}
+	//public void setAction(String action) { this.action = action;}
 }
--- Calculator/src/atj/CalcServlet.java
+++ Calculator/src/atj/CalcServlet.java
@@ -29,10 +29,10 @@ public class CalcServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		//doPost(request,response);
-		
+				
 		HttpSession session = request.getSession();
 		
+		// create Calculator object
 		Calc calculator = (Calc)session.getAttribute("calc");
 		if (calculator == null){
 			calculator = new Calc();
@@ -40,47 +40,66 @@ public class CalcServlet extends HttpServlet {
 			calculator.setResult("0");
 			calculator.setResultDetails("");
 		}
-		
+		// when user click number button (0,1,2,3,4,5,6,7,8,9) or "," button or "C" button
 		if (request.getParameterMap().containsKey("btn")){
-			String btn = request.getParameter("btn");
+			String btn = request.getParameter("btn");	// clicked button (value)
+			// when "C" button was clicked
 			if (btn.equals("C")){
 				calculator.setResult("0");
 				calculator.setResultDetails("");
 				calculator.setA(0);
 				calculator.setB(0);
+				session.removeAttribute("action");
+				session.removeAttribute("score");
 			}
+			// when "," button was clicked
 			else if (btn.equals(",")){
-				// jkjkjkj
+				// dodac obsluge ","
 			}
+			// when number button was clicked (0,1,2,3,4,5,6,7,8,9)
 			else{
-				//calculator.setA(Integer.parseInt(btn));
-				//calculator.setResult(btn);
-				if (calculator.getResult().equals("0")){
+				if (calculator.getResult().equals("0") || calculator.getA() == 0){
 					calculator.setResult("");
 				}
-				
-				
+				if (session.getAttribute("score") != null) { 
+					calculator.setResultDetails(""); 
+					calculator.setResult(""); 
+					session.removeAttribute("score");
+				}
 				calculator.setResult(calculator.getResult() + btn);
-				
-				
+				calculator.setResultDetails(calculator.getResultDetails() + " " + btn);				
 				calculator.setA(Integer.parseInt(calculator.getResult()));
-				calculator.setResultDetails(calculator.getResultDetails() + " " + btn);
 			}
 		}
+		// when user click action button (+,-,*,/,%,sqrt,+/-)
 		if (request.getParameterMap().containsKey("action")){
 			String action = request.getParameter("action");
 			session.setAttribute("action", action);
+			//if (session.getAttribute("score") == null) {session.setAttribute("score",calculator.getA()+calculator.getB()); }
+			calculator.setB(calculator.getA());
+			calculator.setA(0);
 			
-			calculator.setAction(action);
-			calculator.setResultDetails(calculator.getResultDetails() + " " + action);
+			calculator.setResultDetails(calculator.getResultDetails() + " " + action);	
 		}
+		// when user click "=" button
 		if (request.getParameterMap().containsKey("equals")){
-			if (calculator.getAction() != null){
-				calculator.calculate((String)session.getAttribute("action"));
-				calculator.setResultDetails(calculator.getResultDetails() + " =");
+			if (session.getAttribute("action") != null){
+				// action "+" was clicked
+				if (session.getAttribute("action").equals("+")){
+					calculator.add();
+				}
+				// action "-" was clicked
+				// action "*" was clicked
+				// action "/" was clicked
+				// action "%" was clicked
+				// action "sqrt" was clicked
 			}
+			calculator.setResultDetails(calculator.getResultDetails() + " =");
+			session.removeAttribute("action");
+			session.setAttribute("score", Float.parseFloat(calculator.getResult()));
 		}
-			
+		
+		// get calculator view from "calcView.jsp" file
 		RequestDispatcher requestDispatcher = getServletContext().getRequestDispatcher("/calcView.jsp");
 		requestDispatcher.forward(request, response);
 	}
